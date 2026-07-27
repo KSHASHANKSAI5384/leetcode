6407af1e0f71837c9c3b65e2a80fa0172ab2b7f4
KSHASHANKSAI5384 <1nc23ci026@ncetmail.com>
2026-07-27 13:57:54 +0530

    Time: 25 ms (16.75%) | Memory: 29.1 MB (28.17%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -3,17 +3,14 @@ public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         vector<string> ans;
         map<int, string> mymap;
-
-        for (int i = 0; i < names.size(); i++) {
-            mymap[heights[i]] = names[i];
+        for(int i=0;i<names.size();i++){
+            mymap[heights[i]]=names[i];
         }
-
-        for (auto a : mymap) {
+        for(auto a: mymap){
             ans.push_back(a.second);
         }
-
-        reverse(ans.begin(), ans.end());
-
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
+
