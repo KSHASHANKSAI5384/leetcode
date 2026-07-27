adbe3d5e9dabcd2e3a04287a8d8dec5cf33a321e
KSHASHANKSAI5384 <1nc23ci026@ncetmail.com>
2026-07-27 13:56:42 +0530

    Time: 24 ms (17.19%) | Memory: 29.1 MB (28.17%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<string> ans;
+        map<int, string> mymap;
+
+        for (int i = 0; i < names.size(); i++) {
+            mymap[heights[i]] = names[i];
+        }
+
+        for (auto a : mymap) {
+            ans.push_back(a.second);
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
